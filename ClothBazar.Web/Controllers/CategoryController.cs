0840fcb92dd6d870dbcfa4bb8f7eb2398167f3d8
unknown <asi004siddiqui@gmail.com>
2021-03-30 15:58:34 +0500

    Create & Read Categories

---
+++ ClothBazar.Web/Controllers/CategoryController.cs
@@ -0,0 +1,36 @@
+﻿using ClothBazar.Entities;
+using ClothBazar.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClothBazar.Web.Controllers
+{
+    public class CategoryController : Controller
+    {
+        CategoriesService categoryService = new CategoriesService();
+
+
+        // GET: Category
+        public ActionResult Index()
+        {
+            var categories = categoryService.GetCategory();
+            return View(categories);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Category category)
+        {
+            categoryService.SaveCategory(category);
+            return View();
+        }
+    }
+}
